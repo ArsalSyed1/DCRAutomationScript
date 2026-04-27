9675e067c3ba1e425806d5a1026bdfaad6d84e06
Arsal Syed <arsal.syed@teo-intl.com>
2026-04-27 10:01:18 +0500

    Retries and Timeout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -73,7 +73,7 @@ Cypress.Commands.add('waitForPageLoad', (maxWait = 30000) => {
   cy.wait(2000); // Buffer for any remaining requests
 });
 
-/*Cypress.Commands.add('refreshIfStillVisible', (selector, maxRetries = 3, timeout = 60000) => {
+Cypress.Commands.add('refreshIfStillVisible', (selector, maxRetries = 5, timeout = 10000) => {
 
   function check(retriesLeft) {
     cy.get('body').then(($body) => {
@@ -106,26 +106,26 @@ Cypress.Commands.add('waitForPageLoad', (maxWait = 30000) => {
   }
 
   check(maxRetries)
-});*/
+});
 
-Cypress.Commands.add('refreshIfStillVisible', (selector, timeout = 180000) => {
-  cy.log(`Waiting for loader lifecycle: ${selector}`)
+ /* Cypress.Commands.add('refreshIfStillVisible', (selector, timeout = 180000) => {
+    cy.log(`Waiting for loader lifecycle: ${selector}`)
 
-  cy.get('body', { timeout }).then(($body) => {
-    if ($body.find(selector).length) {
-      cy.get(selector, { timeout }).should('not.be.visible')
-    } else {
-      // Wait briefly to see if it appears
-      cy.wait(3000)
+    cy.get('body', { timeout }).then(($body) => {
+      if ($body.find(selector).length) {
+        cy.get(selector, { timeout }).should('not.be.visible')
+      } else {
+        // Wait briefly to see if it appears
+        cy.wait(3000)
 
-      cy.get('body').then(($bodyRetry) => {
-        if ($bodyRetry.find(selector).length) {
-          cy.get(selector, { timeout }).should('not.be.visible')
-        }
-      })
-    }
-  })
-})
+        cy.get('body').then(($bodyRetry) => {
+          if ($bodyRetry.find(selector).length) {
+            cy.get(selector, { timeout }).should('not.be.visible')
+          }
+        })
+      }
+    })
+  })*/
 
 
 
